ba9ca75cad84588eb108b73d0104fef4c69c2e4a
juliajauss <juliajauss@users.noreply.github.com>
2016-04-28 14:56:46 +0200

    x

--- Delete-RSGroup.ps1
+++ Delete-RSGroup.ps1
@@ -3,4 +3,4 @@ Select-AzureRmSubscription -SubscriptionID "00dd302a-92a9-4ec6-a501-213b1bb1fad1
 
 #Delete ResourceGroup
 $resourceGroup = (Get-AzureRmResourceGroup) | Out-GridView -Title "Select a Resource Group" -PassThru
-Remove-AzureRmResourceGroup -Name $resourceG"& 'C:\Users\juliajau\OD\Work\ISV TEAM\Skripte\Delete-RSGroup.ps1' "roup.ResourceGroupName
+Remove-AzureRmResourceGroup -Name $resourceGroup.ResourceGroupName
