70db26bfda1f189879620366accbbc0baab8cd87
juliajauss <jujauss@gmail.com>
2016-04-28 15:03:08 +0200

    update

---
+++ ShutDown-All-VMs.ps1
@@ -0,0 +1,14 @@
+﻿#Login to Azure Account
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionID "<your-subscription-id>"
+
+#All VMs in all Resource groups will be stopped
+$rgs = Get-AzureRmResourceGroup
+foreach ($rg in $rgs)
+{
+    $vms = Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -ResourceType "Microsoft.Compute/virtualMachines"
+    foreach ($vm in $vms)
+    {
+        Stop-AzureRmVM -Name $vm.Name -ResourceGroupName $rg.ResourceGroupName -Force
+    }
+}
