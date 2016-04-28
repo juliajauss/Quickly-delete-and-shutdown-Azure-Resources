4a72c5a50685849d9e3e46136a3410a2cc7916a4
juliajauss <juliajauss@users.noreply.github.com>
2016-04-28 14:57:27 +0200

    modified

--- Shut-Down-All-VMs-In-A-ResourceGroup.ps1
+++ Shut-Down-All-VMs-In-A-ResourceGroup.ps1
@@ -1,6 +1,6 @@
 ﻿#Login to Azure Account
 Login-AzureRmAccount
-Select-AzureRmSubscription -SubscriptionID "00dd302a-92a9-4ec6-a501-213b1bb1fad1"
+Select-AzureRmSubscription -SubscriptionID "<your-subscription-id"
 
 #Input your Resource Group - all VMs in this group will be stopped
 $resourceGroupName = (Get-AzureRmResourceGroup).ResourceGroupName | Out-GridView -Title "Select a Resource Group" -PassThru
